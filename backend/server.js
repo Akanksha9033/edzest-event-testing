b913bf58c70503130706ec699dd1c6b3e365eaad
Akanksha9033 <happychaudhary123spj@gmail.com>
2025-05-24 23:30:44 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://edzest-event-testing-git-master-akanksha9033s-projects.vercel.app' // ✅ Your correct frontend
+    'https://edzest-event-testing-mpblmrujr-akanksha9033s-projects.vercel.app' // ✅ Your correct frontend
    
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
